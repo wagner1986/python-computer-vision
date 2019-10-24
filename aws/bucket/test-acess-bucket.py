25285cfd8eee774e1c608d3ceff23b107e179755
Wagner Luiz <wagner_luiz@id.uff.br>
2019-10-24 09:16:25 -0300

    remover os codigos da aws

--- aws/bucket/test-acess-bucket.py
+++
@@ -1,6 +0,0 @@
-import boto3
-BUCKET = "amazon-rekognition2"
-# Retrieve a bucket's ACL
-s3 = boto3.client('s3')
-result = s3.get_bucket_acl(Bucket=BUCKET)
-print(result)
--- aws/bucket/test-policy-bucket.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-
-BUCKET = "amazon-rekognition2"
-# Retrieve the policy of the specified bucket
-s3 = boto3.client('s3')
-result = s3.get_bucket_policy(Bucket=BUCKET)
-print(result['Policy'])
